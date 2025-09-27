9f564323d23ce57c589e5bedd2503d6d13de70d9
Omar Bamas <jsomar@gmail.com>
2025-09-27 08:10:46 +0300

    omar try to test

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+function TestName() {
+  return (
+    <>
+      <h1>This is Omar App</h1>
+    </>
+  );
+}
+
 function App() {
-  return <h1>Welcome To MY Second App</h1>;
+  return (
+    <>
+      <TestName />
+      <h1>Welcome To MY Second App</h1>
+      <h1>{TestName()}</h1>
+    </>
+  );
 }
 export default App;
